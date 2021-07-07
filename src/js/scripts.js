6e403ac24adafe2e61d58e3441b1b52defea867a
felipelopes94 <felipee.c.lopess@gmail.com>
2021-07-06 23:13:48 -0300

    Testes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,3 +11,20 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
